cd6052a37602565a3169690d9f950fc44f1a2ae7
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-02-28 21:30:06 -0800

    moved main() to sud file, create quit() function and applied to character file.

--- A3/character.py
+++ A3/character.py
@@ -1,21 +1,24 @@
+from sud import user_input_type
+
+
 def character_name() -> str:
     """Obtain input of user-name.
 
     RETURN string of name entered
     """
-    name = input("Enter your name, adventurer.")
+    name = user_input_type("Enter your name, adventurer.")
     name = name.title()
     if not name.isalpha():
         print("I'm sorry, I don't quite understand!")
         return character_name()
 
-    confirm_name = input("Did I get that right, %s (Y/N)?" % name)
+    confirm_name = user_input_type("Did I get that right, %s (Y/N)?" % name)
     if confirm_name.strip().upper() == "N":
         print("I'm sorry!")
         return character_name()
     elif confirm_name.strip().upper() == "Y":
-        print("Welcome to the whimsical and magical SUD, %s!\n"
-              "Before we begin, we will need to create your character.\n"
+        print("Welcome to the magical Pokemon SUD, %s!\n"
+              "Before we begin, you will need to choose your pokemon.\n"
               "From there, the adventure will begin." % name)
         return name
     else:
@@ -28,13 +31,13 @@ def class_selection() -> str:
 
     RETURN selected class from set list.
     """
-    class_filters = ["Cleric", "Warrior", "Thief", "Ranger"]
-    class_input = input("Enter your desired class from the following: " + ", ".join(class_filters))
+    class_type = ["Squirtle", "Charmander", "Bulbasaur"]
+    class_input = user_input_type("Enter your desired Pokemon type from the following: " + ", ".join(class_type))
     class_input = class_input.strip().title()
-    if class_input.strip().title() in class_filters:
+    if class_input.strip().title() in class_type:
         return class_input
     else:
-        print("That is not a familiar class, please try again!")
+        print("That is not a familiar Pokemon type, please try again!")
         return class_selection()
 
 
@@ -42,7 +45,7 @@ def select_gender() -> str:
     """Return selected gender from Male or Female.
 
     RETURN a string of either male or female"""
-    gender_input = input("Please choose your gender:\n1. Male\n2. Female\n")
+    gender_input = user_input_type("Please choose your gender:\n1. Male\n2. Female\n")
     if "Male" in gender_input.strip().title():
         return "Male"
     elif "Female" in gender_input.strip().title():
@@ -56,22 +59,16 @@ def character_information() -> dict:
     """Store all user input into a list.
 
     RETURN dictionary of character information"""
-    character = {"Name": character_name(), "Class": class_selection(), "Gender": select_gender(), "HP": 10}
-    print("Hello, %s the %s! The goal of this SUD is to roam the earth, defeating all monsters encountered.\n"
-          "If you encounter a monster, you may flee or flight; however, the battle will be a combat to the death.\n"
+    character = {"Name": character_name(), "Class": class_selection(),
+                 "Gender": select_gender(), "Position": [0, 0], "HP": 10}
+    
+    print("Hello, %s the %s! The goal of this world is to roam the regions, defeating all Pokemon encountered.\n"
+          "If you encounter a Pokemon, you may flee or flight; however, the battle will be a combat to the death.\n"
           "Fear not, if you leave a battle wounded, 1HP will be added to your health until you reach a max of 10HP,\n"
           "for every move where you are not in battle. "
-          "Additionally, if you choose to flee from a battle, \nthere is a chance that the monster will attack at "
+          "Additionally, if you choose to flee from a battle, \nthere is a chance that your opponent will attack at "
           "the same time; keep this in mind moving forward.\n"
           "You may only move in directions of North, East, South and West within the constraints of our \n"
           "fantastic world and at any time, enter 'quit' to end the game. Good Luck!"
-          % (list(character.values())[0], list(character.values())[1]))
+          % (character["Name"], character["Class"]))
     return character
-
-
-def main():
-    character_information()
-
-
-if __name__ == '__main__':
-    main()
