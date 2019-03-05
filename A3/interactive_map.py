ce5be03e53fa29d8c99a772cf3b2e04f9c3188c2
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-03-04 18:04:16 -0800

    renamed dungeon_map to interactive_map; doctest for pokemon_map

---
+++ A3/interactive_map.py
@@ -0,0 +1,42 @@
+import doctest
+
+
+def pokemon_map(pokemon: dict):
+    """Interpret a square map with user location and available spots.
+
+    PRECONDITION pokemon must be a well-formed dictionary seen in character_information
+    >>> pokemon_map({"Name": "Dave", "Class": "Bulbasaur", "Position": [2, 3], "HP": 10})
+    X X X X
+    X X X X
+    X X X X
+    X X P X
+    >>> pokemon_map({"Name": "Fred", "Class": "Charmander", "Position": [0, 0], "HP": 8})
+    P X X X
+    X X X X
+    X X X X
+    X X X X
+    >>> pokemon_map({"Name": "Henry", "Class": "Squirtle", "Position": [3, 3], "HP": 3})
+    X X X X
+    X X X X
+    X X X X
+    X X X P
+    """
+    map_positions = [["X", "X", "X", "X"],
+                     ["X", "X", "X", "X"],
+                     ["X", "X", "X", "X"],
+                     ["X", "X", "X", "X"]]
+
+    pokemon_x = pokemon["Position"][0]
+    pokemon_y = pokemon["Position"][1]
+    map_positions[pokemon_y][pokemon_x] = "P"
+
+    for row in map_positions:
+        print(" ".join(row))
+
+
+def main():
+    doctest.testmod()
+
+
+if __name__ == '__main__':
+    main()
