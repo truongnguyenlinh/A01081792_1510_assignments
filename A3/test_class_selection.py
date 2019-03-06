a6495ade40142ac2b7ac4d6d22b0d764acfd649b
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-03-06 13:58:19 -0800

    unittest for class_selection

---
+++ A3/test_class_selection.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from character import class_selection
+from unittest.mock import patch
+import unittest
+import io
+
+
+class TestClassSelection(TestCase):
+    @patch('builtins.input', side_effect=["Y", "2"])
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_class_selection(self, mock_stdout, mock_user_input):
+        expected_output = "Please enter a number to choose your Pokemon type!\n"
+        class_selection()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
