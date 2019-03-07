6bc784072a37d7cd705eda2438fddd9be8464c37
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-03-06 17:04:20 -0800

    unittest for combat_round

---
+++ A3/test_combat_round.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from combat import combat_round
+import unittest
+from unittest.mock import patch
+import random
+import io
+
+
+class TestCombatRound(TestCase):
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_round_output(self, mock_stdout):
+        random.seed(6)
+        expected_output = "You attacked Pikachu with a slap and he took 5 damage.\n" \
+                          "Success! Your opponent has fainted and you gained 20 prize dollars from your battle.\n\n"
+        opponent = {"Name": "Pikachu", "Class": "Electric", "Attack": "Static", "HP": 5}
+        combat_round(opponent)
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        random.seed()
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_round_type(self, mock_stdout):
+        random.seed(0)
+        opponent = {"Name": "Charizard", "Class": "Fire", "Attack": "Tackle", "HP": 5}
+        combat_round(opponent)
+        self.assertEqual(type(mock_stdout.getvalue()), str)
+        random.seed()
+
+    def test_combat_round(self):
+        opponent = {"Name": "Mew", "Class": "Psychic", "Attack": "Hypnosis", "HP": 5}
+        random.seed(1)
+        with self.assertRaises(SystemExit):
+            combat_round(opponent)
+        random.seed()
