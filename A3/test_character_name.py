46d4f07505c8181fbe5e4bbecc5274274de73aac
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-03-09 22:29:19 -0800

    additional unittest for character_name

--- A3/test_character_name.py
+++ A3/test_character_name.py
@@ -4,7 +4,7 @@
 
 
 from unittest import TestCase
-from character import character_name
+from character import character_name, pokemon
 from unittest.mock import patch
 import unittest
 import io
@@ -14,7 +14,7 @@ class TestCharacterName(TestCase):
     @patch('builtins.input', side_effect=["helios", "Y"])
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_character_name_type(self, mock_stdout, mock_user_input):
-        """Test for expected output dependent on user input."""
+        """Assert for expected output dependent on user input."""
         expected_output = "Welcome to the magical Pokemon SUD, Helios!\n" \
                           "Before we begin, you will need to choose your pokemon.\n"\
                           "From there, the adventure will begin.\n\n"
@@ -24,10 +24,16 @@ class TestCharacterName(TestCase):
     @patch('builtins.input', side_effect=["TYSON", "n", "RICK","y"])
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_character_name_type(self, mock_stdout, mock_user_input):
-        """Test for expected output dependent on user input."""
+        """Assert for expected output dependent on user input."""
         expected_output = "I'm sorry!\n"\
                           "Welcome to the magical Pokemon SUD, Rick!\n" \
                           "Before we begin, you will need to choose your pokemon.\n" \
                           "From there, the adventure will begin.\n\n"
         character_name()
         self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch('builtins.input', side_effect=["dave", "y"])
+    def test_character_name_global(self, mock_user_input):
+        """Assert that global variable is updated with user input."""
+        character_name()
+        self.assertEqual(pokemon, {'Name': 'Dave', 'Position': [0, 0], 'HP': 10})
