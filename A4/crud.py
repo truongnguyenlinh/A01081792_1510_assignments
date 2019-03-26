438ce02be017a10e60df466f5452d0b866089c95
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-03-25 18:03:32 -0700

    validate user standing updated to now take boolean attribute

---
+++ A4/crud.py
@@ -0,0 +1,84 @@
+import student
+
+
+def confirm_standing():
+    """Confirm student's current standing."""
+    input_standing = input("Is the student in good standing (Y/N)?")
+    if input_standing.strip().upper() == "Y":
+        return True
+    elif input_standing.strip().upper() == "N":
+        return False
+    else:
+        print("Please enter one of the listed options by number!")
+
+
+def add_student():
+    """Obtain user input to generate a Student instance."""
+    first_name = input("Enter the student's first name.")
+    last_name = input("Enter the student's last name.")
+    student_num = input("Enter the student's id number.")
+    student_instance = student.Student(first_name, last_name, student_num, confirm_standing())
+
+
+def file_write(student_object: object):
+    """Append student_object to the end of students.txt file.
+
+    PARAM student_object must be an instance of class Student
+    RETURN True if student_object was successfully appended to file, False otherwise"""
+    filename = "students.txt"
+    with open(filename, "a") as file_object:
+        try:
+            file_object.write(str(student_object) + "n")
+            return True
+        except AttributeError: #some type of error
+            return False
+
+
+def file_read():
+    """Create Student object from each record in file."""
+    student_list = []
+    file_object = open("students.txt", "r")
+    for line in file_object:
+        line.split()
+        first_name = line[0]
+        last_name = line[1]
+        student_num = line[3]
+        student_standing = line[4]
+        grades = line[4:]
+        student_instance = student.Student(first_name, last_name, student_num, student_standing)
+        student_instance.get_final_grades().append(grades)
+        student_list.append(student_instance)
+    return student_list
+
+
+def user_selection(user_input: str):
+    """Determine user input to perform action.
+
+    PRECONDITION: user_input must be an int."""
+    if user_input == "1":
+        pass
+    # add student to students.txt
+    elif user_input == "2":
+        pass
+    # delete student from students.txt
+    elif user_input == "3":
+        pass
+    # calculate class average
+    elif user_input == "4":
+        pass
+    # print class list
+    elif user_input == "5":
+        quit()
+    else:
+        print("Please enter a valid value!\n")
+
+
+def main():
+    while True:
+        print("1. Add student\n2. Delete student\n3. Calculate class average\n4. Print class list\n5. Quit\n")
+        action_input = input("Enter a menu number that represents your desired action:\n")
+        user_selection(action_input)
+
+
+if __name__ == '__main__':
+    main()
--- A4/student.py
+++ A4/student.py
@@ -1,6 +1,6 @@
 class Student:
 
-    def __init__(self, f_name: str, l_name: str, student_num: str, status: str):
+    def __init__(self, f_name: str, l_name: str, student_num: str, status: bool):
         if not (f_name.isalpha() and l_name.isalpha()):
             raise ValueError("Please enter a first and last name with alphabetic characters!")
         else:
@@ -10,27 +10,18 @@ class Student:
         if not (student_num[0].upper() == "A" and student_num[1:].isdigit() and len(student_num) == 9):
             raise ValueError("Enter a nine character student number beginning in 'A' and remaining values as digits.")
         else:
-            self.id = student_num.title()
-        self.standing = self.confirm_standing(status)
-        self.status = status
+            self.id = student_num.upper()
+        self.status = None
+        self.confirm_standing(status)
         self.final_grades = []
 
-    def confirm_standing(self, status: str):
-        if status.strip() == "in-good-standing":
-            return True
-        elif status.strip() == "academic probation":
-            return False
+    def confirm_standing(self, status: bool):
+        if status is True:
+            self.status = "in-good-standing"
+        elif status is False:
+            self.status = "academic probation"
         else:
             raise ValueError("Please enter whether the student is 'in-good-standing' or on 'academic probation'.")
-
-    def gather_grades(self):
-        grades = input("Please enter your grade in percentages in the following format:\nGrade1 Grade2 Grade3 ...")
-        grades = grades.split()
-        try:
-            for grade in grades:
-                self.final_grades.append(int(grade))
-        except ValueError:
-            print("Please enter valid grade-percentages in the given format!")
 
     def get_final_grades(self):
         return self.final_grades
@@ -38,15 +29,13 @@ class Student:
     def get_status(self):
         return self.status
 
-    def print_info(self):
-        print(self.first_name, self.last_name, self.id, self.status, self.standing, self.final_grades)
+    def return_info(self):
+        return self.first_name, self.last_name, self.id, self.status, self.final_grades
 
 
 def main():
-    student_one = Student("angus", "mundy", "a01081792", "in-good-standing")
-    student_one.print_info()
-    student_one.gather_grades()
-    student_one.print_info()
+    student_one = Student("angus", "mundy", "a01081792", True)
+    print(student_one.return_info())
 
 
 if __name__ == '__main__':
