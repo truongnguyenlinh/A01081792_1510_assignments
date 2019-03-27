1925e03ad0d5aee11d48ab27994adfda566bc001
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-03-26 23:37:10 -0700

    getters and setters created in Student class

--- A4/student.py
+++ A4/student.py
@@ -4,24 +4,36 @@ class Student:
         if not (f_name.isalpha() and l_name.isalpha()):
             raise ValueError("Please enter a first and last name with alphabetic characters!")
         else:
-            self.first_name = f_name.title()
-            self.last_name = l_name.title()
+            self.first_name = f_name.strip().title()
+            self.last_name = l_name.strip().title()
 
         if not (student_num[0].upper() == "A" and student_num[1:].isdigit() and len(student_num) == 9):
             raise ValueError("Enter a nine character student number beginning in 'A' and remaining values as digits.")
         else:
             self.id = student_num.upper()
         self.status = status
-        self.confirm_standing()
         self.final_grades = []
 
-    def confirm_standing(self):
-        if self.status is True:
-            self.status = "in-good-standing"
-        elif self.status is False:
-            self.status = "academic probation"
-        else:
-            raise ValueError("Please enter whether the student is 'in-good-standing' or on 'academic probation'.")
+    def append_grades(self, grade):
+        grades_list = self.get_final_grades()
+        grades_list.append(grade)
+        return self.get_final_grades()
+
+    def string_grades(self):
+        grades_list = self.get_final_grades()
+        grades_string = ""
+        for grade in grades_list:
+            grades_string = grades_string + str(grade) + " "
+        return grades_string
+
+    def get_f_name(self):
+        return self.first_name
+
+    def get_l_name(self):
+        return self.last_name
+
+    def get_id(self):
+        return self.id
 
     def get_standing(self):
         return self.status
@@ -31,12 +43,3 @@ class Student:
 
     def return_info(self):
         return self.first_name, self.last_name, self.id, self.status, self.final_grades
-
-
-def main():
-    student_one = Student("angus", "mundy", "a01081792", True)
-    print(student_one.return_info())
-
-
-if __name__ == '__main__':
-    main()
