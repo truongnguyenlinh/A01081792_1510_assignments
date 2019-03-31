967457f3132dce6038ce24d8929daf67ce3f9883
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-03-30 17:38:38 -0700

    unit test for confirm standing

--- A4/test_confirm_unique.py
+++ A4/test_confirm_unique.py
@@ -10,3 +10,9 @@ class TestConfirmUnique(TestCase):
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_confirm_unique(self, mock_stdout):
         self.assertEqual(confirm_unique("A00000000"), "A00000000")
+
+    @patch('builtins.input', side_effect=["A10374653"])
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_confirm_unique(self, mock_stdout, mock_user_input):
+        self.assertEqual(confirm_unique("A09387121"), "A10374653")
+
