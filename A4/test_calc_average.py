7bb093c605dd9de52fc61ef6ef4e29e74f5749c4
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-04-04 11:00:39 -0700

    additional unittest for calc_average.

--- A4/test_calc_average.py
+++ A4/test_calc_average.py
@@ -5,17 +5,28 @@ import io
 
 
 class TestCalcAverage(TestCase):
-    @patch('builtins.open', new_callable=mock_open, read_data="Mundy Angus A12345678 False 59 78 89\n"
-                                                              "Zhao Jason A00000000 True")
+    @patch('builtins.open', new_callable=mock_open, read_data="Angus Mundy A12345678 False 59 78 89\n"
+                                                              "Jason Zhou A00000000 True")
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_calc_average(self, mock_stdout, mock_open):
         expected = "The class average is 75.33.\n\n"
         calc_average()
         self.assertEqual(mock_stdout.getvalue(), expected)
 
-    @patch('builtins.open', new_callable=mock_open, read_data="Last First A00000000 False")
+    @patch('builtins.open', new_callable=mock_open, read_data="First Last A00000000 False")
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_calc_average_none(self, mock_stdout, mock_open):
         expected = "The class currently has no grades!\n"
         calc_average()
         self.assertEqual(mock_stdout.getvalue(), expected)
+
+    @patch('builtins.open', new_callable=mock_open, read_data="First Last A00000000 False")
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_calc_average_type(self, mock_stdout, mock_open):
+        calc_average()
+        self.assertEqual(type(mock_stdout.getvalue()), str)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_calc_average_error(self, mock_stdout):
+        with self.assertRaises(FileNotFoundError):
+            calc_average()
