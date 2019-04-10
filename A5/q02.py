b11a77ba741e5c0eeda876ff1b01926bb379b619
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-04-10 12:05:10 -0700

    doctest added for gcd

--- A5/q02.py
+++ A5/q02.py
@@ -1,17 +1,27 @@
-def gcd(a: int, b: int):
+import doctest
+
+
+def gcd(a: int, b: int) -> int:
     """Return the greatest common divisor of a and b.
 
-    RETURN the largest number that divides cleanly between a and b """
+    RETURN the largest number that divides cleanly between a and b
+    >>> gcd(1, 7)
+    1
+    >>> gcd(2, 4)
+    2
+    >>> gcd(10, 189)
+    1"""
     if a == 0:
         raise ValueError("Please enter a non-zero integer!")
     else:
-        if (a > 0 or a < 0) and b == 0:
+        if b == 0:
             return a
         else:
             return gcd(b, a % b)
 
 
 def main():
+    doctest.testmod()
     print(gcd(8, 7))
     print(gcd(2, 0))
 
